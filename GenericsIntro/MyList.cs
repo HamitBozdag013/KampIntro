cf5eb058e2cf11b5ca2dca36c99fdf69c1515310
Hamit Bozdağ <bozdag.013@gmail.com>
2021-11-09 06:14:24 +0300

    Update

--- GenericsIntro/MyList.cs
+++ GenericsIntro/MyList.cs
@@ -17,15 +17,17 @@ namespace GenericsIntro
 
         public void Add(T item)
         {
-            T[] tempArray = items; //önceki elemanları tutar
+            T[] tempArray = items; //önceki dizi elemanlarını tutar
             items = new T[items.Length + 1]; //items dizisindeki eleman sayısının 1 fazlası kadar hafızada yeni items dizisi için yer açar.
             for(int i=0; i<tempArray.Length; i++)
             {
-                items[i] = tempArray[i]; //items dizisine önceki elemanları geri atanır.
+                items[i] = tempArray[i]; //items dizisine önceki dizi elemanları geri atanır.
             }
 
             items[items.Length - 1] = item; //items dizisine son elemanı ekledik.
 
         }
+
+      
     }
 }
